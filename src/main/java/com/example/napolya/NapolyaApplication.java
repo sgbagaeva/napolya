84738c6cc8e224158c958c1cf777b752ba618012
svetabagaeva5 <sgbagaeva.work@gmail.com>
2024-11-16 22:20:46 +0300

    spring-приложение

---
+++ src/main/java/com/example/napolya/NapolyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.napolya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapolyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NapolyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/napolya/NapolyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.napolya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapolyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
